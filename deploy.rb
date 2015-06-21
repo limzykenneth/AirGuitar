3fcd0696ca09a2b7f65cb5926dcd7da442de938a
limzykenneth <limzy.kenneth@gmail.com>
2015-06-21 12:18:30 +0100

    Added Ruby deployment script

---
+++ deploy.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+require 'logger'
+
+$LOG = Logger.new('git-deploy.log',0,100*1024*1024)
+
+post '/payload', :agent => /GitHub-Hookshot/ do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  commands = ['git status','git pull origin master','git submodule sync','git submodule update','git submodule status']
+  commands.each do |i|
+  	system i
+  end
+  
+  $LOG.info "Git deploy ran without critical error."
+  return "Git deploy ran without critical error."
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
