8ccb477cb41c2dd36f7c5f57c2475fef6b4e9f3e
limzykenneth <limzy.kenneth@gmail.com>
2015-06-25 16:17:44 +0100

    Fixing deploy.php to make it secure

--- deploy.php
+++ deploy.php
@@ -7,19 +7,17 @@
 	 *		https://gist.github.com/1809044
 	 */
 
-	// The commands
-	function parseRequestHeaders() {
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) <> 'HTTP_') {
-            continue;
-        }
-        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-        $headers[$header] = $value;
-    }
-    return $headers;
-	}
+	$request=$_POST;
+	var_dump($request);
 
+	function verify_request(){
+		$message = "12345";
+		$key     = getenv("GIT_TOKEN");
+	    $hash    = hash_hmac("sha1", $key, $message);
+	    var_dump($hash);
+	}
+	
+	// The commands
 	$commands = array(
 		'echo $PWD',
 		'whoami',
@@ -30,33 +28,9 @@
 		'git submodule status',
 	);
 
-	// Run the commands for output
-	$output = '';
+	// Run the commands
 	foreach($commands AS $command){
 		// Run it
 		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
 	}
-
-	// Make it pretty for manual user access (and why not?)
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
-<?php echo $output;?>
-</pre>
-</body>
-</html>
