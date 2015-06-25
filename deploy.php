f5a5b3f01c3b4b5a987708e0873bd4bac1486ff8
limzykenneth <limzy.kenneth@gmail.com>
2015-06-25 17:04:40 +0100

    Fixing deploy.php to make it secure

--- deploy.php
+++ deploy.php
@@ -9,19 +9,23 @@
 
 	$agent=$_SERVER['HTTP_USER_AGENT'];
 	$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+	$body=http_get_request_body();
+
 	base64_encode($agent);
 	base64_encode($signature);
 	if (strpos($agent,'GitHub-Hookshot') !== false){
 		error_log($agent);
 		error_log($signature);
+		error_log(verify_request());
 	}else{
 		error_log('Request header is invalid.');
 	}
 
 	function verify_request(){
-		$message = "12345";
+		$message = $body;
 		$key     = getenv("GIT_TOKEN");
 	    $hash    = hash_hmac("sha1", $key, $message);
+	    return $hash;
 	}
 
 	// The commands
